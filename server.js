2dede606d1a41a1224f8fcbda99561b13d0e04c1
SV20008 <SV20008@ues.edu.sv>
2026-06-02 22:45:13 -0600

    configuracion para deploy

--- server.js
+++ server.js
@@ -185,8 +185,8 @@ app.post("/generar", upload.single("imagen"), (req, res) => {
 });
 
 // Iniciar servidor
-app.listen(3000, () => {
-    console.log(
-        "Servidor corriendo en http://localhost:3000"
-    );
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
 });
